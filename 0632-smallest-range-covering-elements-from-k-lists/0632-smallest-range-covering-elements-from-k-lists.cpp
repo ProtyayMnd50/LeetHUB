261ce94fc4e5e01d92eabee4d7a28ba371af6cd7
Protyay Mondal <96626338+ProtyayMnd50@users.noreply.github.com>
2024-10-13 23:36:18 +0530

    Time: 38 ms (84.45%), Space: 22.5 MB (48.27%) - LeetHub

--- 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
@@ -1,40 +1,86 @@
 class Solution {
+private:
+    bool chk(int a,int b, int c, int d){
+        // if new numbers are answer
+        if(b-a < d-c){
+            return true;
+        }
+        else if(b-a == d-c){
+            if(a<c){return true;}
+        }
+
+        // new number dont have minimum difference
+        // uptill now
+        return false;
+    }
 public:
-    vector<int> smallestRange(vector<vector<int>>& nums) {
-        // Min-Heap: stores (value, list index, element index)
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> minHeap;
-        int curMax = numeric_limits<int>::min();
+    vector<int> smallestRange(vector<vector<int>>& a) {
+        int ansx = -1e5, ansy = 1e5; 
+        int total_list = a.size();
 
-        // Initialize the heap with the first element of each list
-        for (int i = 0; i < nums.size(); i++) {
-            minHeap.push({nums[i][0], i, 0});
-            curMax = max(curMax, nums[i][0]);
-        }
-        // Track the smallest range
-        vector<int> smallRange = {0, numeric_limits<int>::max()};
+        // UNCOMMENT TO CHECK HOW GIVEN ARRAY PRINT AFTER SORT
+        // sort(a.begin(), a.end());
+        //     cout<<"[";
+        // for(int i=0;i<total_list;i++){
+        //     cout<<"[";
+        //     for(int j=0;j<a[i].size();j++){
+        //         cout<<a[i][j]<<((j==a[i].size()-1)?"":",");
+        //     }
+        //     cout<<"]"<<((i==total_list-1)?"":",");
+        // }
+        //     cout<<"]";
+        // cout<<endl;
+        // cout<<endl;
 
-        while (!minHeap.empty()) {
-            // Get the minimum element from the heap
-            vector<int> curr = minHeap.top();
-            minHeap.pop();
-            int curMin = curr[0], listIdx = curr[1], elemIdx = curr[2];
-
-            // Update the smallest range if a better one is found
-            if (curMax - curMin < smallRange[1] - smallRange[0]) {
-                smallRange[0] = curMin;
-                smallRange[1] = curMax;
-            }
-
-            // Move to the next element in the same list
-            if (elemIdx + 1 < nums[listIdx].size()) {
-                int nextVal = nums[listIdx][elemIdx + 1];
-                minHeap.push({nextVal, listIdx, elemIdx + 1});
-                curMax = max(curMax, nextVal);
-            } else {
-                // If any list is exhausted, stop
-                break;
+        vector<pair<int,int>> vp;
+        for(int i=0;i<total_list;i++){
+            for(int j=0;j<a[i].size();j++){
+                vp.push_back({a[i][j],i});
             }
         }
-        return smallRange;
+
+
+        sort(vp.begin(), vp.end());
+
+        // UNCOMMENT TO CHECK HOW VECTOR IS PRINTING AFTER SORT
+        // int cnt=0;
+        // cout<<"[";
+        // for(auto& val:vp){
+        //     cout<<"["<<val.first<<","<<val.second<<"]";
+        //     cnt++;
+        //     if(cnt!=vp.size()){cout<<",";}
+        // }
+        // cout<<"]";
+
+        unordered_map<int,int> um;
+        int l=0, r=0;
+        int n = vp.size();
+        while(r<n){
+            int num = vp[r].first;
+            int list = vp[r].second;
+
+            um[list]++;
+
+            while(um.size() >= total_list){
+                //    |-----new range---------| |--old range--|
+                if(chk(vp[l].first,vp[r].first,   ansx, ansy )){
+                    // cout<<"Possible ans = "<<ansx<<" "<<ansy<<endl;
+                    ansx = vp[l].first;
+                    ansy = vp[r].first;
+                    // cout<<"Possible ans = "<<ansx<<" "<<ansy<<endl;
+                }
+
+                int old_list = vp[l].second;
+                
+                um[old_list]--;
+                l++;
+
+                if(um[old_list]==0){um.erase(old_list);}
+            }
+
+            r++;
+        }
+
+        return {ansx, ansy};
     }
 };
