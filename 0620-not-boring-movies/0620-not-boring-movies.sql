1e0c53bafabf86554f6bf4c76f8ff7612ec6e8c9
Protyay Mondal <96626338+ProtyayMnd50@users.noreply.github.com>
2024-10-22 01:01:58 +0530

    Time: 932 ms (5.01%), Space: 0B (100%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 with tab1 as
 (select * from
-Cinema where id%2=1 and description not in('boring')
+Cinema where mod(id,2)=1 and description!='boring'
 )
 select * from tab1 order by rating desc;
