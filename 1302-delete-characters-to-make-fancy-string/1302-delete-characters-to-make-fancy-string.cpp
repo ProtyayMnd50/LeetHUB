69540b7ed4f1236867bccb6cde1ac887ebfbe653
Protyay Mondal <96626338+ProtyayMnd50@users.noreply.github.com>
2024-11-02 19:35:53 +0530

    Time: 1859 ms (5.26%), Space: 56.7 MB (5.17%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
@@ -1,18 +1,41 @@
 class Solution {
 public:
-    static string makeFancyString(string& s) {
-        int n=s.size(), i=0;
-        char prev='@';
-        for(int len=0; char c: s){
-            if (prev!=c) len=1;
-            else len++;
-            if (len<= 2) 
-                s[i++]=c;
-            prev=c;
+    string makeFancyString(string s) {
+        int n=s.size();
+        vector<char>str;
+        for(auto &x:s)
+        str.push_back(x);
+
+        int i=0;
+        string res="";
+        for(;i<str.size();){
+            int j=i;
+            int cnt=0;
+            while(j<str.size()&&str[i]==str[j]){
+                j++;
+                cnt++;
+            }
+            cnt=j-i;
+            if(cnt>=3){
+                // for(int k=i+2;k<min(j,n);k++){
+                //     str[i]='^';
+                // }
+                res+=str[i];
+                res+=str[i+1];
+                // cout<<i+2<<"\n";
+            }else{
+                res+=str[i];
+                if(cnt==2)
+                res+=str[i+1];
+            }
+            // cout<<cnt<<" ";
+            // cout<<i<<" "<<j-1<<"\n";
+            i=j;
             
         }
-        s.resize(i);
-        return s;
-         
+      for(auto &x:res)
+      cout<<x;
+
+        return res;
     }
 };
