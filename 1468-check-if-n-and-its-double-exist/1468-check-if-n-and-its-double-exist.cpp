1e9b52e40d91bdee280e2ce4c17abc31f827fa51
Protyay Mondal <96626338+ProtyayMnd50@users.noreply.github.com>
2024-12-21 18:50:03 +0530

    Time: 5 ms (10.62%), Space: 15.1 MB (8.28%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.cpp
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     bool checkIfExist(vector<int>& arr) {
-        unordered_map<int, int> mp; // HashMap to store the occurrence of elements
-
-        for (int i : arr) { // Iterate over the array
-            // Check if 2 * i or i / 2 already exists in the map
-            // Note: We use a special condition for i / 2 to ensure it's valid
-            if (mp[2 * i] || (!(i & 1) && mp[i / 2])) {
-                return true; // Return true if the condition is met
-            }
-            // Add the current number to the map
-            mp[i]++;
+        int n=arr.size();
+        unordered_map<int,int>mp;
+        for(int i=0;i<n;i++){
+            mp[arr[i]]++;
         }
+        // cout<<mp[0]<<endl;
+        if(mp[0]>=2)return true;//even number of zeros
 
-        return false; // Return false if no such pair exists
+        for(int i=0;i<n;i++){
+            if(mp[2*arr[i]]>0 && arr[i]!=0)return true;
+        }
+        return false;
     }
 };
