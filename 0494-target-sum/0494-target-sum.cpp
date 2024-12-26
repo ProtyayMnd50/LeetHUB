c84d4b643335f3b870945e6b650d506cf8769b5d
Protyay Mondal <96626338+ProtyayMnd50@users.noreply.github.com>
2024-12-26 22:41:50 +0530

    Time: 919 ms (12.48%), Space: 11.5 MB (78.96%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,28 +1,17 @@
 class Solution {
 public:
-    int func(int ind,vector<int>&nums,int target,vector<vector<int>>&dp){
-        if(ind==0){
-            if(nums[0]==0&&target==0)return 2;//tk or not tk two ways
-            else if(nums[0]==target&&nums[0]!=0)return 1;//tk only, 1 way possible
-            else if(target==0)return 1;//nt anything
-            else return 0;
-        }
-            if(dp[ind][target]!=-1)return dp[ind][target];
-            int nt=func(ind-1,nums,target,dp);
-            int tk=0;
-            if(nums[ind]<=target)
-            tk=func(ind-1,nums,target-nums[ind],dp);
-            return tk+nt;
-        
-    }
     int findTargetSumWays(vector<int>& nums, int target) {
-        int n=nums.size();
-        int tot=accumulate(nums.begin(),nums.end(),0);
-        int d=target;
-        int targ=tot-d;
-        if(targ<0||targ%2==1)return 0;//cannot be partitioned
-        targ>>=1;// (tot-d)/2
-        vector<vector<int>>dp(n,vector<int>(targ+1,-1));
-        return func(n-1,nums,targ,dp);
+        return cal(nums, target, 0, 0);
+    }
+
+    int cal(vector<int>& nums, int tar, int idx, int currSum) {
+        if (idx == nums.size()) {
+            return (currSum == tar) ? 1 : 0;
+        }
+
+        int add = cal(nums, tar, idx + 1, currSum + nums[idx]);
+        int sub = cal(nums, tar, idx + 1, currSum - nums[idx]);
+
+        return add + sub;
     }
 };
