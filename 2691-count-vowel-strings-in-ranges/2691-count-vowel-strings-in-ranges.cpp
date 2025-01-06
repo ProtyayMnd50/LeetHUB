0599e5777042f5a3c4f7e0d30cbd7404ec62e9e5
Protyay Mondal <96626338+ProtyayMnd50@users.noreply.github.com>
2025-01-07 00:38:23 +0530

    Time: 6 ms (44.11%), Space: 70.3 MB (40.37%) - LeetHub

--- 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.cpp
+++ 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
     vector<int> vowelStrings(vector<string>& words, vector<vector<int>>& queries) {
-        int n = words.size();
-        vector<int> Prefix(n + 1, 0);
-        unordered_set<char> vowels = {'a', 'e', 'i', 'o', 'u'}; 
-        for (int i = 0; i < n; i++) {
-            Prefix[i + 1] = Prefix[i];
-            if (vowels.count(words[i].front()) && vowels.count(words[i].back())) {
-                Prefix[i + 1]++;  
-            }
+        int n=words.size();
+        vector<int>hsh(n,0);
+        vector<char> vow={'a','e','i','o','u'};
+        vector<int>pref(n,0);
+        for(int i=0;i<words.size();i++){
+            int p1=find(vow.begin(),vow.end(),words[i].front())-vow.begin();//first letter is vowel if p1 != -1
+            int p2=find(vow.begin(),vow.end(),words[i].back())-vow.begin();//last letter is vowel if p2 != -1
+            if(p1<5 && p2<5 )//both are vowels 
+            hsh[i]++;
+            // reqs.push_back(i);
         }
-        vector<int> ANS; 
-        for (auto& query : queries) {
-            int L = query[0], R = query[1];  
-            ANS.push_back(Prefix[R + 1] - Prefix[L]);  
+        pref[0]=hsh[0];
+        for(int i=1;i<n;i++){
+            pref[i]=hsh[i]+pref[i-1];
         }
+        vector<int>ans;
+        for(int i=0;i<queries.size();i++){
+            int l=queries[i][0];
+            int r=queries[i][1];
+            ans.push_back(l==0?pref[r]:pref[r]-pref[l-1]);
+        }
+        return ans;
 
-        return ANS;  
     }
 };
