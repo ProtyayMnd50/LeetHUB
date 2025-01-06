8372bcee9343761ba57a8b22c664289d6882acd4
Protyay Mondal <96626338+ProtyayMnd50@users.noreply.github.com>
2025-01-07 01:56:14 +0530

    Time: 2 ms (66.71%), Space: 13.3 MB (5.93%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -2,22 +2,41 @@
 class Solution {
 public:
     vector<int> minOperations(string boxes) {
-        // int ans=0;
         int n=boxes.size();
-        vector<int>full;//stores indexes of full boxes
-        for(int i=0;i<n;i++){
-            if(boxes[i]=='1')full.push_back(i);
+        vector<int>px(n,0);
+        vector<int>sx(n,0);//suffix sums of indexes 
+        int balls=count(boxes.begin(),boxes.end(),'1');//total '1' in the vector
+        px[0]=(boxes[0]=='1'?0:0);
+        for(int i=1;i<n;i++){
+            px[i]=(boxes[i]=='1'?i:0);
+            px[i]+=px[i-1];
         }
-       
+        sx.back()=(boxes.back()=='1'?n-1:0);
+        for(int i=n-2;i>=0;i--){
+            sx[i]=(boxes[i]=='1'?i:0);
+            sx[i]+=sx[i+1];
+        }
         vector<int>ans;
+        int l1=0;
         for(int i=0;i<n;i++){
-            int jmp=0;
-            for(int j=0;j<full.size();j++){
-                jmp+=abs(i-full[j]);
-            }
-            ans.push_back(jmp);
+            if(boxes[i]=='1')l1++;
+            int lops=abs(i*l1-px[i]);
+          
+            int rops=abs(i*(balls-l1)-sx[i]);
+            if(boxes[i]=='1')
+            rops-=i;
+           ans.push_back(lops+rops);
         }
-        // cout<<ans<<nl;
+   
         return ans;
+
+
+        for(auto &x:px)
+        cout<<x<<" ";
+        cout<<nl;
+        for(auto &x:sx)
+        cout<<x<<" ";
+        cout<<nl;
+        return {0};
     }
 };
