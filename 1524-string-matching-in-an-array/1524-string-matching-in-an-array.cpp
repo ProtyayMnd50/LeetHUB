86d0c8b7a62594ff1f430c4ee07b8be7fbaf526b
Protyay Mondal <96626338+ProtyayMnd50@users.noreply.github.com>
2025-01-17 08:57:52 +0530

    Time: 0 ms (100%), Space: 11.4 MB (52.9%) - LeetHub

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.cpp
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.cpp
@@ -7,13 +7,8 @@ public:
         vector<string>ans;
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-               if(words[i].compare(words[j])!=0){
-                int pos=words[j].find(words[i]);
-                int sz=words[j].size();
-                if(pos>=0&&pos<sz)
-                // ans.push_back(words[i]);
-                st.insert(words[i]);
-               }
+               if(i!=j &&words[j].find(words[i])!=-1)
+               st.insert(words[i]);
             }
         }
 
