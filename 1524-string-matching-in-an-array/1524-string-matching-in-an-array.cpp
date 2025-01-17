a94c8de233ac7751894cdacb2ac2dfe201c19c02
Protyay Mondal <96626338+ProtyayMnd50@users.noreply.github.com>
2025-01-17 08:51:18 +0530

    Time: 3 ms (49.22%), Space: 11.4 MB (72.85%) - LeetHub

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.cpp
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.cpp
@@ -1,16 +1,25 @@
+#define nl "\n"
 class Solution {
 public:
     vector<string> stringMatching(vector<string>& words) {
-        vector<string> result;
-        int n = words.size();
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < n; j++) {
-                if(i != j && words[j].find(words[i]) != string::npos) {
-                    result.push_back(words[i]);
-                    break;
-                }
+        int n=words.size();
+        unordered_set<string>st;
+        vector<string>ans;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+               if(words[i].compare(words[j])!=0){
+                int pos=words[j].find(words[i]);
+                int sz=words[j].size();
+                if(pos>=0&&pos<sz)
+                // ans.push_back(words[i]);
+                st.insert(words[i]);
+               }
             }
         }
-        return result;
+
+        for(auto &x:st){
+            ans.push_back(x);
+        }
+        return ans;
     }
 };
