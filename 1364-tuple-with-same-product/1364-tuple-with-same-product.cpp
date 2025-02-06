1c61c4a0a814dec661ff2257b867722b424ec34d
Protyay Mondal <96626338+ProtyayMnd50@users.noreply.github.com>
2025-02-06 23:08:41 +0530

    Time: 347 ms (28.84%), Space: 85 MB (67.71%) - LeetHub

--- 1364-tuple-with-same-product/1364-tuple-with-same-product.cpp
+++ 1364-tuple-with-same-product/1364-tuple-with-same-product.cpp
@@ -2,24 +2,24 @@ class Solution {
 public:
     int tupleSameProduct(vector<int>& nums) {
         int n=nums.size();
-        map<int,int> mp;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
+        unordered_map<int,int>mp;
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
                 mp[nums[i]*nums[j]]++;
             }
         }
-        int cnt=0;
         int ans=0;
         for(auto &x:mp){
-            if(x.second>=4){
-            cnt++;
-            cout<<x.first<<" "<<x.second/2<<endl;
-            int freq=x.second/2;
-            int perm=freq*(freq-1);
-            perm>>=1;
-            ans+=perm*8;
+            if(x.second>=2){
+                // cout<<x.first<<" "<<x.second<<endl;
+                int freq=x.second;
+                int val=freq*(freq-1);
+                val>>=1;
+                ans+=val*8;
             }
         }
         return ans;
+
+        return 0;
     }
 };
