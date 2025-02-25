3e49bcff34994cd21c018b43228555e6442c7a4a
Protyay Mondal <96626338+ProtyayMnd50@users.noreply.github.com>
2025-02-25 23:22:16 +0530

    Time: 0 ms (100%), Space: 27.8 MB (22.75%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.cpp
+++ 3429-special-array-i/3429-special-array-i.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     bool isArraySpecial(vector<int>& nums) {
-      int n=nums.size();
-      if(n==1)
-      return true;
-
-      for(int i=0;i<n-1;i++){
-        int pr=nums[i]+nums[i+1];
-        if(pr%2==0)
-        return false;
-      }  
-      return true;
+        int n=nums.size();
+        for(int i=0;i<n-1;i++){
+            int sum=nums[i]+nums[i+1];
+            if(!(sum&1))
+            return false;
+        }
+        return true;
     }
 };
