e638d64b962aa6597c21c4f3d86dc9cde61fdf59
Protyay Mondal <96626338+ProtyayMnd50@users.noreply.github.com>
2025-02-26 00:26:33 +0530

    Time: 15 ms (3.25%), Space: 8.8 MB (5.43%) - LeetHub

--- 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.cpp
+++ 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     bool areAlmostEqual(string s1, string s2) {
-        int n=s1.size();
         map<pair<char,char>,int>mp;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<s1.size();i++){
             if(s1[i]!=s2[i]){
                 mp[{s1[i],s2[i]}]++;
             }
         }
-        if(mp.empty())return true;
-        else if(mp.size()==2){
-            for(auto &x:mp){
-                char fe=x.first.first;
-                char se=x.first.second;
-                if(mp[{fe,se}]==1&&mp[{se,fe}]==1)return true;
-                else return false;
-            }
-        }else
-        return false;
+        int cnt=0;
+        for(auto &x:mp){
+            char c1=x.first.first;
+            char c2=x.first.second;   
+            cout<<c1<<", "<<c2<<"->"<<x.second<<endl;
 
-        return true;
+            if(mp[{c1,c2}]==mp[{c2,c1}])
+            cnt+=x.second;
+        }
+        cout<<cnt<<endl;
+        return ( (cnt==2 && mp.size()==2)||mp.empty())?true:false;
     }
 };
