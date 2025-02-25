3c7c5e3189fd71d32da9c6df1e45091e3a7fb207
Protyay Mondal <96626338+ProtyayMnd50@users.noreply.github.com>
2025-02-26 00:58:15 +0530

    Time: 300 ms (85.88%), Space: 85.1 MB (46.6%) - LeetHub

--- 1364-tuple-with-same-product/1364-tuple-with-same-product.cpp
+++ 1364-tuple-with-same-product/1364-tuple-with-same-product.cpp
@@ -5,21 +5,20 @@ public:
         unordered_map<int,int>mp;
         for(int i=0;i<n-1;i++){
             for(int j=i+1;j<n;j++){
-                mp[nums[i]*nums[j]]++;
+                int prod=nums[i]*nums[j];
+                mp[prod]++;
             }
         }
         int ans=0;
         for(auto &x:mp){
-            if(x.second>=2){
-                // cout<<x.first<<" "<<x.second<<endl;
-                int freq=x.second;
-                int val=freq*(freq-1);
-                val>>=1;
-                ans+=val*8;
-            }
+            // cout<<x.first<<"->"<<x.second<<endl;
+            int freq=x.second;
+            int comb=freq*(freq-1);
+            comb>>=1;
+            
+            ans+=comb*8;
+            
         }
         return ans;
-
-        return 0;
     }
 };
