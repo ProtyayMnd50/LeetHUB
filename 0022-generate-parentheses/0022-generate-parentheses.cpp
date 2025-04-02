aade3cc7ff234b3207722b144b54321b37bd1392
Protyay Mondal <96626338+ProtyayMnd50@users.noreply.github.com>
2025-04-02 10:04:25 +0530

    Time: 1 ms (86.96%), Space: 16.3 MB (23.17%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,21 +1,34 @@
 class Solution {
 public:
-    void generateParenthesisHelper(int open, int close, string current, vector<string>& result) {
-    if (open == 0 && close == 0) {
-        result.push_back(current);
-        return;
+    void recur(int open,int close,string curr,vector<string>&res,int n){
+        //open->number of opening brackets that can be added
+
+        //close->number of closing brackets that can be added
+
+        int copen=n-open;
+        int cclose=n-close;
+        if(open==0&&close==0){
+            res.push_back(curr);
+            return;
+        }
+        
+        if(open>0){
+            curr.push_back('(');
+            recur(open-1,close,curr,res,n);
+            curr.pop_back();
+        }
+
+        if(copen>cclose){
+            curr.push_back(')');
+            recur(open,close-1,curr,res,n);
+            curr.pop_back();
+        }
     }
-
-    if (open > 0)
-        generateParenthesisHelper(open - 1, close, current + "(", result);
-
-    if (close > open)
-        generateParenthesisHelper(open, close - 1, current + ")", result);
-}
-
     vector<string> generateParenthesis(int n) {
-        vector<string> result;
-    generateParenthesisHelper(n, n, "", result);
-    return result;
+        vector<string>res;
+        recur(n,n,"",res,n);
+
+        return res;
+
     }
 };
