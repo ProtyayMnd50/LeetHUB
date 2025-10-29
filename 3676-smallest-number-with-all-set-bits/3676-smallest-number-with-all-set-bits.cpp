62c7aded4a406750eafc55d47395fc63ca8ec26d
Protyay Mondal <96626338+ProtyayMnd50@users.noreply.github.com>
2025-10-30 01:59:59 +0530

    Time: 2 ms (7.67%), Space: 8.7 MB (11.64%) - LeetHub

--- 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.cpp
+++ 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.cpp
@@ -1,13 +1,18 @@
 #define nl "\n"
 class Solution {
 public:
-    int smallestNumber(int n) {
-        for(int i=n;;i++){
-            int chk=i&(i+1);
-            if(!chk) {
-                cout<<i<<nl;
-                return i;
-            }
+    vector<int> allOnes;
+    Solution() {
+        for (int i = 1; i <= 1023; i++) {
+            int chk = i & (i + 1);
+            if (!chk)
+                allOnes.push_back(i);
         }
+    }
+
+    int smallestNumber(int n) {
+        int ansPos =
+            lower_bound(allOnes.begin(), allOnes.end(), n) - allOnes.begin();
+        return allOnes[ansPos];
     }
 };
