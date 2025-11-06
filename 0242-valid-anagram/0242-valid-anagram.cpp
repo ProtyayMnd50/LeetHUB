e0b23d4183070c5e2751db412c02ce7a33b70418
Protyay Mondal <96626338+ProtyayMnd50@users.noreply.github.com>
2025-11-07 01:05:59 +0530

    Time: 0 ms (100%), Space: 9.8 MB (48.28%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,36 +1,18 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        map<char,int> mp1;
-        map<char,int> mp2;
-        for(auto &x:s){
-            mp1[x]++;
-        }
-        for(auto &x:t){
-            mp2[x]++;
-        }
-        if(mp1.size()!=mp2.size()){
-            return false;
-        }else{
-            vector<pair<char,int>>p1;
-            vector<pair<char,int>>p2;
-            for(auto &x:mp1){
-                p1.push_back({x.first,x.second});
-            }
-            for(auto &x:mp2){
-                p2.push_back({x.first,x.second});
-            }
-            bool res=true;
-            for(int i=0;i<mp1.size();i++){
-                if(p1[i].first==p2[i].first&&p1[i].second==p2[i].second){
+       //solution involving use of a single hashMap for the purpose
+        if(s.size()!=t.size()) return false;
 
-                }else{
-                    res=false;
-                    break;
-                }
+        vector<int>hsh(26,0);
+        for(int i=0;i<s.size();i++){
+            hsh[s[i]-'a']++;
+            hsh[t[i]-'a']--;
+        }
 
-            }
-            return res;
-        }    
+        for(int i=0;i<26;i++){
+            if(hsh[i]!=0) return false;
+        }
+        return true;
     }
 };
